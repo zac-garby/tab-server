2fb2c4f819c41ce0917c74372e457242dd9c2a9f
Zac Garby <me@zacgarby.co.uk>
2018-11-05 20:47:50 +0000

    Get a basic web server going

--- main.go
+++ main.go
@@ -1,5 +1,16 @@
 package main
 
+import "github.com/Zac-Garby/tab-server/src"
+
 func main() {
-	
+	// Make a new Server instance, addr = "" and
+	// port = 8000. Everything else is zero values
+	// of the respective types.
+	s := &src.Server{
+		Address: "",
+		Port:    8000,
+	}
+
+	// Start listening on port 8000.
+	s.Listen()
 }
---
+++ src/server.go
@@ -0,0 +1,65 @@
+package src
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+// A Server is used to handle HTTP requests.
+type Server struct {
+	// Address is the address to listen on, usually an empty string
+	// which indicates that any address will be listened to.
+	Address string
+
+	// Port is the port to listen on.
+	Port int
+
+	// HTTPS says whether or not HTTPS should be used to communicate.
+	HTTPS bool
+
+	// Certificate is the filename of the HTTPS certificate.
+	Certificate string
+
+	// Key is the filename of the HTTPS key.
+	Key string
+}
+
+// Listen starts the HTTP server running on the given address and port.
+func (s *Server) Listen() {
+	// Create a new router, which will be used to listen to HTTP requests and decide
+	// what to do the respond back.
+	r := mux.NewRouter()
+
+	r.HandleFunc("/", s.handleIndex)
+	r.HandleFunc("/settings", s.handleSettings)
+
+	// Starts the HTTP server listening using the router defined previously.
+	fmt.Printf("Server is running at %s:%d...\n", s.Address, s.Port)
+	http.ListenAndServe(fmt.Sprintf("%s:%d", s.Address, s.Port), r)
+}
+
+// handleIndex is called to respond to a HTTP request to /.
+func (s *Server) handleIndex(w http.ResponseWriter, r *http.Request) {
+	//  Disable caching for this route.
+	w.Header().Set("Cache-Control", "max-age=0")
+
+	// Shorthand for:
+	//  - opening www/html/index.html
+	//  - reading its contents
+	//  - serving that text, along with relavent metadata
+	http.ServeFile(w, r, "www/html/index.html")
+}
+
+// handleSettings is called to respond to a HTTP request to /settings.
+func (s *Server) handleSettings(w http.ResponseWriter, r *http.Request) {
+	//  Disable caching for this route.
+	w.Header().Set("Cache-Control", "max-age=0")
+
+	// Shorthand for:
+	//  - opening www/html/settings.html
+	//  - reading its contents
+	//  - serving that text, along with relavent metadata
+	http.ServeFile(w, r, "www/html/settings.html")
+}
