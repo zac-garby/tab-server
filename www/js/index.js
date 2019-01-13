e0a2e736a5f7e1b55e8b36cfa7795bc231630e75
Zac Garby <me@zacgarby.co.uk>
2019-01-13 15:38:19 +0000

    Sorting & searching

--- www/js/index.js
+++ www/js/index.js
@@ -1,3 +1,5 @@
+var tabs = []
+
 // This function will be called after the DOM has been completely
 // loaded, meaning that the DOM elements can be referenced from
 // inside this function.
@@ -20,22 +22,12 @@ function updateTabList() {
             // successful. 200 = OK.
             if (this.status == 200) {
                 // Parse the JSON response into a list of objects.
-                var tabs = JSON.parse(this.responseText)
+                tabs = JSON.parse(this.responseText)
 
-                // Find the tab-list element by its ID and remove all
-                // of its children by setting its inner HTML source to
-                // an empty string.
-                var ul = document.getElementById("tab-list")
-                ul.innerHTML = ""
-
-                // For each tab in the list of tabs, append a new <li>
-                // element with its title and artist's name to the tab
-                // list element.
-                for (var tab of tabs) {
-                    var li = document.createElement("li")
-                    li.innerHTML = tab.title + " - " + tab.artist
-                    ul.appendChild(li)
-                }
+                // Show the tabs which are now stored in the global
+                // variable, taking into account the sorting and
+                // filtering options.
+                showTabs()
             } else {
                 // If the execution gets here, an error has occured. Thus,
                 // send an error message to the user via an alert.
@@ -52,4 +44,83 @@ function updateTabList() {
     // the page while the request is loading.
     req.open("GET", location.origin + "/api/tabs", true)
     req.send()
+}
+
+function showTabs() {
+    // Get the user's selected sort type from the sorting combo box.
+    // This will be one of:
+    //  - title-asc
+    //  - title-desc
+    //  - artist-asc
+    //  - artist-desc
+    var sortType = document.getElementById("sorting").value
+    var searchTerm = document.getElementById("search-bar").value
+
+    // Get an array of the tabs which are to be displayed by sorting
+    // and filtering the entire list of tabs.
+    var tabsToDisplay = tabs.sort(sortFunction(sortType))
+                     .filter(filterFunction(searchTerm))
+
+    // Find the tab-list element by its ID and remove all
+    // of its children by setting its inner HTML source to
+    // an empty string.
+    var ul = document.getElementById("tab-list")
+    ul.innerHTML = ""
+
+    // For each tab in the list of tabs, append a new <li>
+    // element with its title and artist's name to the tab
+    // list element.
+    for (var tab of tabsToDisplay) {
+        var li = document.createElement("li")
+        li.innerHTML = tab.title + " - " + tab.artist
+        ul.appendChild(li)
+    }
+}
+
+// sortFunction returns a function which compares two tab
+// objects, based on the given sortOption.
+// sortOption can be one of:
+//  - title-asc
+//  - title-desc
+//  - artist-asc
+//  - artist-desc
+function sortFunction(sortOption) {
+    switch (sortOption) {
+    case "title-asc":
+        return (a, b) => a.title > b.title
+    case "title-desc":
+        return (a, b) => a.title < b.title
+    case "artist-asc":
+        return (a, b) => a.artist > b.artist
+    case "artist-desc":
+        return (a, b) => a.artist < b.artist
+    default:
+        return () => 0
+    }
+}
+
+// filterFunction returns a function which returns true
+// if an object should be in the filtered array, and false
+// otherwise, based on the search term.
+function filterFunction(searchTerm) {
+    // If the search term is empty, return a function which
+    // always will just return true.
+    if (searchTerm == "") {
+        return () => true
+    }
+
+    // Extract a list of words from the search term by first
+    // removing whitespace from either end of the string and
+    // then using the split function with the regex /\s+/ to
+    // split the string by whitespace.
+    var words = searchTerm.trim().split(/\s+/)
+
+    // Return a function which will only return true if all
+    // of the words are present in the argument's title or
+    // artist. This is a closure so this scope is still
+    // available to the returned function, such that 'words'
+    // will be in scope.
+    return (s) => words.every(word =>
+        s.title.toLowerCase().match(word.toLowerCase()) ||
+        s.artist.toLowerCase().match(word.toLowerCase()))
 }
